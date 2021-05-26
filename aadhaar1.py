211bd84a2ca2942a150709e6bfef7ebd1766f8a6
Your Name <jackgopi494@gmail.com>
2021-05-26 05:48:41 +0530

    Check Aadhaar biometric registeration eligibility using exception

---
+++ aadhaar1.py
@@ -0,0 +1,29 @@
+import datetime
+choose='y'
+while choose.upper()=='Y':
+    userdata = input('Please Enter Your DOB (dd/mm/yyyy) :')
+    try:
+        birthday = datetime.datetime.strptime(userdata,'%d/%m/%Y').date()
+        #print(birthday)
+    except ValueError:
+        print("You entered wrong date or wrong format ,Please enter correct format dd/mm/yyy")
+        continue 
+    currentDate = datetime.date.today()
+    #print(currentDate)
+    days =currentDate - birthday
+    #print(type(days))
+    inte=days.days
+    #print(type(int))
+    if inte > 1830:
+        print("You Can Take a Aadhaar With Bio-Metric Data")
+    else:
+        print("You Can't Take a Aadhaar With Bio-Metric Data")
+    while True:
+        ch=input("Enter Y to continue and N to Exit:")
+        if ch.upper()=='Y' or ch.upper()=='N':
+            choose=ch
+            break
+        else:
+            print("Please Enter The Correct Choose  :")
+            continue
+        
